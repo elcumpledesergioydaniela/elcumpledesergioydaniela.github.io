98ace9d1a0bdeab907b76a300b16d12294466bd3
deleitempanadas <deleite.empanadas2012@gmail.com>
2023-10-15 01:15:01 -0600

    eight commit

--- js/demo003.js
+++ js/demo003.js
@@ -60,7 +60,7 @@ window.sr = ScrollReveal();
         z: 100
     },
 		duration: 2500, //tiempo
-		delay:3500,
+		delay:3000,
 		origin: 'top', //origen
 		scale: 0.10,
 		distance: '-0' //distancia
@@ -597,3 +597,44 @@ $(document).ready(function(){
         return false; 
     }); 
 });
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
